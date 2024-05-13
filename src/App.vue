4163253dd0c899f7eb03151a1fbd3afd0ca25fdc
Sean Kelliher <sbkelliher@icloud.com>
2024-05-13 08:34:39 -0400

    Remove NavPart and AboutAsos components.

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,12 @@
 <script setup>
 import HeaderPart from "./components/HeaderPart.vue";
-import NavPart from "./components/NavPart.vue";
 import ResultsPart from "./components/ResultsPart.vue";
-import AboutAsos from "./components/AboutAsos.vue";
 </script>
 
 <template>
     <div id="container">
         <HeaderPart />
-        <NavPart />
         <ResultsPart />
-        <AboutAsos />
     </div>
 </template>
 
