3b470c8d5e9cc0cd2dcf61876c04dfe2bfc4e047
Sean Kelliher <sbkelliher@icloud.com>
2024-05-13 08:33:12 -0400

    Add initial structure and content to AboutAsos.vue.

--- src/components/AboutAsos.vue
+++ src/components/AboutAsos.vue
@@ -3,7 +3,10 @@
 </script>
 
 <template>
-    <p>This is the "about ASOS" component.</p>
+    <div class="about-asos">
+        <h3>About ASOS</h3>
+        <p>The Automated Surface Observing Systems (ASOS) program continuously monitors weather at sites (mostly airports) throughout the United States. It is a joint effort of the National Weather Service (NWS), the Federal Aviation Administration (FAA), and the Department of Defense (DOD).</p>
+    </div>
 </template>
 
 <style scoped>
