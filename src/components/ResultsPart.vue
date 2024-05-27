c35d898198e8794788c68957ff6b161f17057a9d
Sean Kelliher <sbkelliher@icloud.com>
2024-05-27 14:03:03 -0400

    Reorder components, elements. Make ref array to store selected airports into reactive object, add sample data.

--- src/components/ResultsPart.vue
+++ src/components/ResultsPart.vue
@@ -1,8 +1,8 @@
 <script setup>
 import { airports } from "../data/asos-list.js";
-import NavPart from "./NavPart.vue";
+import AirportForm from "./AirportForm.vue";
 import AboutAsos from "./AboutAsos.vue";
-import { ref } from "vue";
+import { ref, reactive } from "vue";
 
 // Input boxes for "Airport Code" - single, combined.
 const ac1 = ref("");
@@ -32,36 +32,75 @@ function clearCode() {
 }
 
 // User clicks "+" symbol, invoke addAirport function, save "added" airport.
-const addedAirports = ref([]);
+const addedAirports = reactive({
+    depart: {
+        id: "kjfk",
+        name: "John F. Kennedy International Airport",
+        city: "New York",
+        state: "NY",
+        type: "Departing"
+    },
+    stop1: {
+        id: "kord",
+        name: "Chicago O'Hare International Airport",
+        city: "Chicago",
+        state: "IL",
+        type: "Stopover"
+    },
+    stop2: {
+        id: "",
+        name: "",
+        city: "",
+        state: "",
+        type: "Stopover"
+    },
+    stop3: {
+        id: "",
+        name: "",
+        city: "",
+        state: "",
+        type: "Stopover"
+    },
+    dest: {
+        id: "ksfo",
+        name: "San Francisco International Airport",
+        city: "San Francisco",
+        state: "CA",
+        type: "Destination"
+    }
+});
+
 function addAirport() {
     addedAirports.value.push(returnedAirport.value);
 }
+
+
 </script>
 
 <template>
-    <NavPart
-        v-model:asos-code1="ac1"
-        v-model:asos-code2="ac2"
-        v-model:asos-code3="ac3"
-        v-model:asos-code4="ac4"
-        @run-code="runCode"
-        @clear-code="clearCode"
-    />
-    <main>
+    <nav>
+        <AirportForm
+            v-model:asos-code1="ac1"
+            v-model:asos-code2="ac2"
+            v-model:asos-code3="ac3"
+            v-model:asos-code4="ac4"
+            @run-code="runCode"
+            @clear-code="clearCode"
+        />
         <div class="returned-airports">
-            <div class="airport">
-                <div class="airport-details">
+            <div class="returned-airport">
+                <div class="returned-airport-details">
                     <dl>
                         <dt>{{ airports[`${returnedAirport}`].name }} - {{ airports[`${returnedAirport}`].id }}</dt>
                         <dd>{{ airports[`${returnedAirport}`].city }}, {{ airports[`${returnedAirport}`].stateAbr.toUpperCase() }}</dd>
                     </dl>
                 </div>
-                <div class="airport-btns">
-                    <button class="btn-flight-type departing">Departing</button>
-                    <button class="btn-flight-type stopover">Stopover</button>
-                    <button class="btn-flight-type destination">Destination</button>
+                <div class="returned-airport-btns">
+                    <button class="returned-btn-flight-type departing">Departing</button>
+                    <button class="returned-btn-flight-type stopover">Stopover</button>
+                    <button class="returned-btn-flight-type destination">Destination</button>
                 </div>
-                <div class="airport-icon">
+                <div class="returned-airport-icon">
                     <figure class="icon">
                         <img
                             @click="addAirport()"
@@ -71,23 +110,57 @@ function addAirport() {
                     </figure>
                 </div>
             </div>
-            <AboutAsos />
+            <!--<AboutAsos />-->
         </div>
+    </nav>
+    <main>
+
 
         <div class="added-airports">
             <div class="added-airport">
-                {{ addedAirports }}
+                <div class="added-airport-type departing">
+                    <dl>
+                        <dt>{{ addedAirports.depart.type }}</dt>
+                    </dl>
+                </div>
+                <div class="added-airport-details">
+                    <dl>
+                        <dt>{{ addedAirports.depart.id }}</dt>
+                        <dd>{{ addedAirports.depart.name }}</dd>
+                        <dd>{{ addedAirports.depart.city}}, {{ addedAirports.depart.state }}</dd>
+                    </dl>
+                </div>
             </div>
-            <div
-                v-for="addedAirport in addedAirports"
-                :key="addedAirport"
-                class="added-airport"
-            >
-                <dl>
-                    <dt>{{ airports[addedAirport].id }}</dt>
-                    <dd>{{ airports[addedAirport].name }}</dd>
-                    <dd>{{ airports[addedAirport].city }}, {{ airports[addedAirport].stateAbr.toUpperCase() }}</dd>
-                </dl>
+
+
+            <div class="added-airport">
+                <div class="added-airport-type stopover">
+                    <dl>
+                        <dt>{{ addedAirports.stop1.type }}</dt>
+                    </dl>
+                </div>
+                <div class="added-airport-details">
+                    <dl>
+                        <dt>{{ addedAirports.stop1.id }}</dt>
+                        <dd>{{ addedAirports.stop1.name }}</dd>
+                        <dd>{{ addedAirports.stop1.city}}, {{ addedAirports.stop1.state }}</dd>
+                    </dl>
+                </div>
+            </div>
+
+            <div class="added-airport">
+                <div class="added-airport-type destination">
+                    <dl>
+                        <dt>{{ addedAirports.dest.type }}</dt>
+                    </dl>
+                </div>
+                <div class="added-airport-details">
+                    <dl>
+                        <dt>{{ addedAirports.dest.id }}</dt>
+                        <dd>{{ addedAirports.dest.name }}</dd>
+                        <dd>{{ addedAirports.dest.city}}, {{ addedAirports.dest.state }}</dd>
+                    </dl>
+                </div>
             </div>
         </div>
     </main> 
